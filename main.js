d7440624a702f6eb46a681a50fac98ee10010a0f
az198071123 <az198071123@gmail.com>
2019-09-09 03:25:43 +0800

    自動冒煙測試，所有遊戲

--- main.js
+++ main.js
@@ -15,8 +15,41 @@ const chrome = require("selenium-webdriver/chrome");
  *
  * 测试!
  */
-let game_id_map = new Map();
+const config = {
+    browser: 'chrome',
+    deviceName: 'Pixel 2',
+    html_login_url: 'http://103.230.243.68:11898/page/ares_game.htm',
+    user_id: 'test111',
+    gold_id: '0',
+    game_id_map: new Map(),
+    check_script: 'return ares && ares.room["1"] != "ws://127.0.0.1:12001/"'
+    // check_script: 'return ares && ares.socket.sock && ares.socket.sock.readyState == WebSocket.OPEN'
+};
+function push_game_id_map(driver, selector) {
+    let selectList = driver.findElement(selector);
+    return selectList.findElements(selenium_webdriver_1.By.tagName('option'))
+        .then(options => {
+        let options_promises = options.map(option => {
+            return option.getAttribute("value")
+                .then(game_id => {
+                config.game_id_map.set(game_id, option);
+            });
+        });
+        return Promise.all(options_promises);
+    });
+}
+// function selectOption(driver: ThenableWebDriver, selector: By, item: string) {
+//     console.log('selectOption', item);
+//     let selectList = driver.findElement(selector);
+//     return selectList.click()
+//         .then(() => {
+//             const btn = config.game_id_map.get(item);
+//             if (btn) return btn.click();
+//             else return Promise.reject('game id not found');
+//         })
+// }
 function selectOption(driver, selector, item) {
+    console.log('selectOption', item);
     let selectList = driver.findElement(selector);
     selectList.click();
     return selectList.findElements(selenium_webdriver_1.By.tagName('option'))
@@ -31,53 +64,55 @@ function selectOption(driver, selector, item) {
         return Promise.all(options_promises);
     });
 }
+function start_loop_game(driver) {
+    console.log('start_loop_game for ', config.game_id_map);
+    // config.game_id_map.clear();
+    // config.game_id_map.set('5503', new WebElement(driver, "test"));
+    // config.game_id_map.set('5504', new WebElement(driver, "test"));
+    let loop_game_promises = Promise.resolve();
+    for (let [game_id, btn] of config.game_id_map.entries()) {
+        loop_game_promises = loop_game_promises
+            .then(() => selectOption(driver, selenium_webdriver_1.By.id('game_id'), game_id))
+            .then(() => driver.findElement(selenium_webdriver_1.By.className('btn-login-game'))
+            .then(e => e.click()))
+            .then(() => driver.sleep(1000))
+            // await new Promise(resolve => setTimeout(resolve, 1000));
+            .then(() => driver.getAllWindowHandles()
+            .then(handles => driver.switchTo().window(handles[1])))
+            .then(() => driver.wait(() => {
+            return driver.executeScript(config.check_script);
+        }, 50000))
+            .then(() => driver.close())
+            .then(() => driver.sleep(1000))
+            .then(() => driver.getAllWindowHandles()
+            .then(handles => driver.switchTo().window(handles[0])));
+    }
+    return loop_game_promises;
+}
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         console.log('ts hello world');
         const driver = new selenium_webdriver_1.Builder()
-            .forBrowser('chrome')
+            .forBrowser(config.browser)
             .setChromeOptions(new chrome.Options()
-            .setMobileEmulation({ deviceName: 'Pixel 2' }))
+            .setMobileEmulation({ deviceName: config.deviceName }))
             .build();
         return Promise.all([
-            driver.get('http://103.230.243.68:11898/page/ares_game.htm'),
+            driver.get(config.html_login_url),
             driver.findElement(selenium_webdriver_1.By.id('user_id'))
                 .then(e => {
                 e.clear();
-                e.sendKeys('test111');
-            })
-                .catch(() => console.error('find gold_id error')),
+                e.sendKeys(config.user_id);
+            }),
             driver.findElement(selenium_webdriver_1.By.id('gold_id'))
                 .then(e => {
                 e.clear();
-                e.sendKeys('10000');
-            })
-                .catch(() => console.error('find user_id error')),
-            selectOption(driver, selenium_webdriver_1.By.id('game_id'), '5503'),
-            driver.findElement(selenium_webdriver_1.By.id('kind_h'))
-                .then(e => {
-                e.click();
-            })
-                .catch(() => console.error('find kind_h error')),
-        ]).then(() => {
-            return driver.findElement(selenium_webdriver_1.By.className('btn-login-game'))
-                .then(e => {
-                e.click();
-            })
-                .catch(() => console.error('find btn-login-game error'));
-        }).then(() => __awaiter(this, void 0, void 0, function* () {
-            // await new Promise(resolve => setTimeout(resolve, 1000));
-            yield driver.sleep(1000);
-            return driver.getAllWindowHandles()
-                .then(handles => {
-                driver.switchTo().window(handles[1]);
-            });
-        })).then(() => {
-            return driver.wait(() => {
-                return driver.executeScript("return ares && ares.socket.sock && ares.socket.sock.readyState == WebSocket.OPEN");
-            }, 50000)
-                .then(() => console.log('find Cocos2dGameContainer !!'));
-        }).then(() => driver.close());
+                e.sendKeys(config.gold_id);
+            }),
+            driver.findElement(selenium_webdriver_1.By.id('kind_v'))
+                .then(e => e.click()),
+            push_game_id_map(driver, selenium_webdriver_1.By.id('game_id'))
+        ]).then(() => start_loop_game(driver));
     });
 }
-main().catch((e) => console.log('something error', e));
+main().catch((e) => console.log('something error:', e));
--- main.ts
+++ main.ts
@@ -1,12 +1,47 @@
-import { Builder, By, ThenableWebDriver } from 'selenium-webdriver';
+import { Builder, By, ThenableWebDriver, WebElement } from 'selenium-webdriver';
 import * as chrome from 'selenium-webdriver/chrome';
 /**
  * main 入口
  * 
  * 测试!
  */
-let game_id_map = new Map<number, string>();
+const config = {
+    browser: 'chrome',
+    deviceName: 'Pixel 2',
+    html_login_url: 'http://103.230.243.68:11898/page/ares_game.htm',
+    user_id: 'test111',
+    gold_id: '0',
+    game_id_map: new Map<string, WebElement>(),
+    check_script: 'return ares && ares.room["1"] != "ws://127.0.0.1:12001/"'
+    // check_script: 'return ares && ares.socket.sock && ares.socket.sock.readyState == WebSocket.OPEN'
+}
+
+function push_game_id_map(driver: ThenableWebDriver, selector: By) {
+    let selectList = driver.findElement(selector);
+    return selectList.findElements(By.tagName('option'))
+        .then(options => {
+            let options_promises = options.map(option => {
+                return option.getAttribute("value")
+                    .then(game_id => {
+                        config.game_id_map.set(game_id, option);
+                    })
+            });
+            return Promise.all(options_promises);
+        })
+}
+
+// function selectOption(driver: ThenableWebDriver, selector: By, item: string) {
+//     console.log('selectOption', item);
+//     let selectList = driver.findElement(selector);
+//     return selectList.click()
+//         .then(() => {
+//             const btn = config.game_id_map.get(item);
+//             if (btn) return btn.click();
+//             else return Promise.reject('game id not found');
+//         })
+// }
 function selectOption(driver: ThenableWebDriver, selector: By, item: string) {
+    console.log('selectOption', item);
     let selectList = driver.findElement(selector);
     selectList.click();
     return selectList.findElements(By.tagName('option'))
@@ -20,54 +55,58 @@ function selectOption(driver: ThenableWebDriver, selector: By, item: string) {
             return Promise.all(options_promises);
         })
 }
+
+function start_loop_game(driver: ThenableWebDriver) {
+    console.log('start_loop_game for ', config.game_id_map);
+    // config.game_id_map.clear();
+    // config.game_id_map.set('5503', new WebElement(driver, "test"));
+    // config.game_id_map.set('5504', new WebElement(driver, "test"));
+    let loop_game_promises = Promise.resolve();
+    for (let [game_id, btn] of config.game_id_map.entries()) {
+        loop_game_promises = loop_game_promises
+            .then(() => selectOption(driver, By.id('game_id'), game_id))
+            .then(() => driver.findElement(By.className('btn-login-game'))
+                .then(e => e.click()))
+            .then(() => driver.sleep(1000))
+            // await new Promise(resolve => setTimeout(resolve, 1000));
+            .then(() => driver.getAllWindowHandles()
+                .then(handles => driver.switchTo().window(handles[1])))
+            .then(() => driver.wait(() => {
+                return driver.executeScript(config.check_script)
+            }, 50000))
+            .then(() => driver.close())
+            .then(() => driver.sleep(1000))
+            .then(() => driver.getAllWindowHandles()
+                .then(handles => driver.switchTo().window(handles[0])))
+    }
+    return loop_game_promises;
+}
+
 async function main() {
     console.log('ts hello world');
 
     const driver = new Builder()
-        .forBrowser('chrome')
+        .forBrowser(config.browser)
         .setChromeOptions(new chrome.Options()
-            .setMobileEmulation({ deviceName: 'Pixel 2' }))
+            .setMobileEmulation({ deviceName: config.deviceName }))
         .build();
 
     return Promise.all([
-        driver.get('http://103.230.243.68:11898/page/ares_game.htm'),
+        driver.get(config.html_login_url),
         driver.findElement(By.id('user_id'))
             .then(e => {
                 e.clear();
-                e.sendKeys('test111');
-            })
-            .catch(() => console.error('find gold_id error')),
+                e.sendKeys(config.user_id);
+            }),
         driver.findElement(By.id('gold_id'))
             .then(e => {
                 e.clear();
-                e.sendKeys('10000');
-            })
-            .catch(() => console.error('find user_id error')),
-        selectOption(driver, By.id('game_id'), '5503'),
-        driver.findElement(By.id('kind_h'))
-            .then(e => {
-                e.click();
-            })
-            .catch(() => console.error('find kind_h error')),
-    ]).then(() => {
-        return driver.findElement(By.className('btn-login-game'))
-            .then(e => {
-                e.click();
-            })
-            .catch(() => console.error('find btn-login-game error'))
-    }).then(async () => {
-        // await new Promise(resolve => setTimeout(resolve, 1000));
-        await driver.sleep(1000);
-        return driver.getAllWindowHandles()
-            .then(handles => {
-                driver.switchTo().window(handles[1]);
-            })
-    }).then(() => {
-        return driver.wait(() => {
-            return driver.executeScript("return ares && ares.socket.sock && ares.socket.sock.readyState == WebSocket.OPEN")
-        }, 50000)
-            .then(() => console.log('find Cocos2dGameContainer !!'));
-    }).then(() => driver.close());
+                e.sendKeys(config.gold_id);
+            }),
+        driver.findElement(By.id('kind_v'))
+            .then(e => e.click()),
+        push_game_id_map(driver, By.id('game_id'))
+    ]).then(() => start_loop_game(driver))
 }
 
-main().catch((e) => console.log('something error', e));
+main().catch((e) => console.log('something error:', e));
